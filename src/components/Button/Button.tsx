08fafa715b45df951195b90ec726d8f0b0675916
Ivan Stelmakh <stelmakh.developer@gmail.com>
2022-07-18 13:53:55 +0300

    feat: remove React imports

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
 import styles from './button.styles';
--- src/components/ProductImage/ProductImage.tsx
+++ src/components/ProductImage/ProductImage.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { Image, ImageProps } from 'react-native';
 
 const ProductImage: FC<ImageProps> = (props) => {
