f0b477665c3a2926e66ae4b26527975166f52460
Ivan Stelmakh <stelmakh.developer@gmail.com>
2022-07-19 14:14:35 +0300

    fix: src prop for next image

--- src/components/ProductImage/ProductImage.web.tsx
+++ src/components/ProductImage/ProductImage.web.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 import type { ProductImageProps } from './productImageProps';
 
-const ProductImage: FC<ProductImageProps> = (props) => {
+const ProductImage: FC<ProductImageProps> = ({ source, ...props }) => {
   console.warn('Web ProductImage props: ', props);
-  return <Image {...props} />;
+  return <Image {...props} src={source} />;
 };
 
 export { ProductImage };
