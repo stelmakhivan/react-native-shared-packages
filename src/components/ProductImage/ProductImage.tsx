b6495e6f2ee478e6052456508e5a0f9e8e5a774d
Ivan Stelmakh <stelmakh.developer@gmail.com>
2022-07-19 14:10:44 +0300

    feat: source prop for image

--- src/components/ProductImage/ProductImage.tsx
+++ src/components/ProductImage/ProductImage.tsx
@@ -4,9 +4,9 @@ import { Image } from 'react-native';
 
 import type { ProductImageProps } from './productImageProps';
 
-const ProductImage: FC<ProductImageProps> = (props) => {
+const ProductImage: FC<ProductImageProps> = ({ source, ...props }) => {
   console.warn('Native ProductImage props: ', props);
-  return <Image {...props} />;
+  return <Image {...props} source={{ uri: source }} />;
 };
 
 export { ProductImage };
--- src/components/ProductImage/productImageProps.ts
+++ src/components/ProductImage/productImageProps.ts
@@ -2,4 +2,7 @@ import type { ImageProps as ReactNativeImageProps } from 'react-native';
 
 import type { ImageProps as NextImageProps } from 'next/image';
 
-export type ProductImageProps = NextImageProps & ReactNativeImageProps;
+export type ProductImageProps = Omit<NextImageProps, 'src'> &
+  Omit<ReactNativeImageProps, 'source'> & {
+    source: string;
+  };
